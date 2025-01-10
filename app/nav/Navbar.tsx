387997ef28b5c8d1ef2f0e5199261dca006bd26f
Szabolcs Peter <szabi.peter@gmail.com>
2025-01-10 12:23:18 +0100

    Installed Next Auth, added login functionality

--- app/nav/Navbar.tsx
+++ app/nav/Navbar.tsx
@@ -1,13 +1,14 @@
 import { AiOutlineCar } from "react-icons/ai";
 import Search from "./Search";
 import Logo from "./Logo";
+import LoginButton from "./LoginButton";
 
 export default function Navbar() {
     return (
         <header className="sticky top-0 z-50 flex justify-between bg-white shadow-md py-5 px-5 items-center text-gray-800">
             <Logo />
             <Search />
-            <div>Login</div>
+            <LoginButton />
         </header>
     );
 }
