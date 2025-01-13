a55e30c92098376648065cf5f694f8d8f0d4779b
Szabolcs Peter <szabi.peter@gmail.com>
2025-01-13 10:32:28 +0100

    User actions dropdown populated when logged in

--- app/nav/Navbar.tsx
+++ app/nav/Navbar.tsx
@@ -11,7 +11,7 @@ export default async function Navbar() {
             <Logo />
             <Search />
             {user ? (
-                    <UserActions />
+                    <UserActions user={user}/>
             ) : (
                     <LoginButton />
             )}
